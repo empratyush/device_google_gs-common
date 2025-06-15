27ebf2b8dcc25ec8cafc5c43bb8ed26978bb44d6
Fred Underwood <underwoodfred@proton.me>
2025-06-15 14:44:54 +1000

    strip euiccpixel_app

--- euiccpixel_app/euiccpixel_app_st54.mk
+++ euiccpixel_app/euiccpixel_app_st54.mk
@@ -1,3 +1,2 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/euiccpixel_app/sepolicy/common
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/euiccpixel_app/sepolicy/st54
-PRODUCT_PACKAGES += EuiccSupportPixel-P23
