85e0c2d812febdcae394646984cbac1b2233fd6a
Fred Underwood <underwoodfred@proton.me>
2025-06-15 13:24:25 +1000

    strip bcmbt

--- bcmbt/bluetooth.mk
+++ bcmbt/bluetooth.mk
@@ -1,11 +1,7 @@
-PRODUCT_SOONG_NAMESPACES += vendor/broadcom/bluetooth
 PRODUCT_PACKAGES += \
        android.hardware.bluetooth-V1-ndk.so \
        android.hardware.bluetooth.finder-V1-ndk.so \
        android.hardware.bluetooth.ranging-V1-ndk.so \
-       android.hardware.bluetooth-service.bcmbtlinux \
-       vendor.google.bluetooth_ext-V1-ndk.so \
-       bt_vendor.conf \
        android.hardware.bluetooth.prebuilt.xml \
        android.hardware.bluetooth_le.prebuilt.xml
 
--- bcmbt/dump/dumplog.mk
+++ bcmbt/dump/dumplog.mk
@@ -1,3 +1,1 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/bcmbt/dump/sepolicy/
-
-PRODUCT_PACKAGES_DEBUG += dump_bcmbt
