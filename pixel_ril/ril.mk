04c0b5c4bc20b487f44d0061d02c35f862b7fc83
Fred Underwood <underwoodfred@proton.me>
2025-06-15 16:34:42 +1000

    strip pixel_ril

--- pixel_ril/ril.mk
+++ pixel_ril/ril.mk
@@ -7,12 +7,4 @@ else
 endif
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/pixel_ril/compatibility_matrix.xml
 
-PRODUCT_PACKAGES += ril-extension
-
-PRODUCT_PACKAGES_DEBUG += libgooglerilmemmonitor
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/google/tools/ril-extension-service \
-    vendor/google/tools/ril-mem-monitor
-
 USE_GOOGLE_RIL_EXT := true
