1a05d883aebb0a12f8c839af23c14fcd87455734
Fred Underwood <underwoodfred@proton.me>
2025-06-15 14:36:08 +1000

    strip display

--- display/dump.mk
+++ display/dump.mk
@@ -1,4 +1,1 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/display/sepolicy
-
-PRODUCT_PACKAGES_DEBUG += dump_display_userdebug.sh
-PRODUCT_PACKAGES += dump_display
--- display/dump_second_display.mk
+++ display/dump_second_display.mk
@@ -1,3 +1,1 @@
-PRODUCT_PACKAGES += dump_second_display
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/display/sepolicy_second_display
-
