0c365f432d21a44014afe57f9f4dd8ea35e39f70
Fred Underwood <underwoodfred@proton.me>
2025-06-15 17:42:58 +1000

    strip soc

--- soc/freq.mk
+++ soc/freq.mk
@@ -1,4 +1,1 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/soc/sepolicy/freq
-
-PRODUCT_PACKAGES += dump_devfreq
-
--- soc/soc.mk
+++ soc/soc.mk
@@ -1,5 +1,1 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/soc/sepolicy/soc
-
-PRODUCT_PACKAGES += dump_soc
-PRODUCT_PACKAGES_DEBUG += dump_memory
-
