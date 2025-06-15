512bfaa4b16e6daf2c86adffee1c1439912d562f
Fred Underwood <underwoodfred@proton.me>
2025-06-15 17:39:29 +1000

    strip sensors

--- sensors/sensors.mk
+++ sensors/sensors.mk
@@ -1,5 +1,4 @@
 $(call inherit-product-if-exists, vendor/google/sensors/usf/android/usf_efw_product.mk)
 
-PRODUCT_PACKAGES += dump_sensors
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/sensors/sepolicy
 
