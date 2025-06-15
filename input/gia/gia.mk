a2c3fbcad291b913dae0cd08132c563bc4025e28
Fred Underwood <underwoodfred@proton.me>
2025-06-15 15:58:05 +1000

    add gia TODO

--- input/gia/gia.mk
+++ input/gia/gia.mk
@@ -1,5 +1,6 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/input/gia/sepolicy
 
+# TODO: This probably should be removed, but need to verify it doesn't exist. Difficult to grep for.
 PRODUCT_PACKAGES += gia
 
 DEVICE_MANIFEST_FILE += device/google/gs-common/input/gia/aidl/manifest.xml
