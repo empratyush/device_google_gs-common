dee1bbc8254ce45ac1877f46a4b3a90baa02c967
Fred Underwood <underwoodfred@proton.me>
2025-06-15 13:18:03 +1000

    strip battery_mitigation

--- battery_mitigation/bcl.mk
+++ battery_mitigation/bcl.mk
@@ -1,21 +1,3 @@
-ifeq (,$(filter factory_%,$(TARGET_PRODUCT)))
-PRODUCT_PACKAGES += battery_mitigation
-endif
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-ifeq (,$(filter factory_%,$(TARGET_PRODUCT)))
-PRODUCT_PACKAGES += BrownoutDetection
-endif
-endif
-
-ifeq ($(RELEASE_PIXEL_AIDL_BATTERY_MITIGATION_HAL), true)
-PRODUCT_PROPERTY_OVERRIDES += \
-       vendor.battery_mitigation.aidl.enable=true
-endif
-
-PRODUCT_SOONG_NAMESPACES += device/google/gs-common/battery_mitigation \
-                            vendor/google/battery_mitigation
-PRODUCT_PACKAGES += vendor.google.battery_mitigation-default
-PRODUCT_PACKAGES += vendor.google.battery_mitigation.service_static
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/battery_mitigation/compatibility_matrix.xml
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/battery_mitigation/sepolicy/vendor
