599b0d74b7aba4592c70b4d80ef05eceb1527084
Fred Underwood <underwoodfred@proton.me>
2025-06-15 17:44:04 +1000

    strip sota_app

--- sota_app/factoryota-watch.mk
+++ sota_app/factoryota-watch.mk
@@ -1,4 +1,1 @@
-PRODUCT_PACKAGES += \
-    FactoryOtaWearPrebuilt
-
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs-common/sota_app/sepolicy/system_ext
--- sota_app/factoryota.mk
+++ sota_app/factoryota.mk
@@ -1,4 +1,1 @@
-PRODUCT_PACKAGES += \
-    FactoryOtaPrebuilt
-
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs-common/sota_app/sepolicy/system_ext
