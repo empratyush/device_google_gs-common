08861acb6b04e9578235fd86149747e1b20a283a
Fred Underwood <underwoodfred@proton.me>
2025-06-15 13:27:30 +1000

    strip betterbug

--- betterbug/betterbug.mk
+++ betterbug/betterbug.mk
@@ -1,8 +1,2 @@
-# When neither AOSP nor factory targets
-ifeq (,$(filter aosp_% factory_%, $(TARGET_PRODUCT)))
-  PRODUCT_PACKAGES += BetterBugStub
-  PRODUCT_PACKAGES_DEBUG += $(RELEASE_PACKAGE_BETTER_BUG)
-endif
-
 PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs-common/betterbug/sepolicy/product/public
 PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs-common/betterbug/sepolicy/product/private
