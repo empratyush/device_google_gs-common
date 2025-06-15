2e54f58f07b0f3f107172abde7fd6a08fea9c13b
Fred Underwood <underwoodfred@proton.me>
2025-06-15 12:30:42 +1000

    strip aoc

--- aoc/aoc.mk
+++ aoc/aoc.mk
@@ -1,19 +1,4 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/aoc/sepolicy
 
-PRODUCT_PACKAGES += dump_aoc \
-		    aocd \
-		    aocxd
-
-PRODUCT_COPY_FILES += \
-	device/google/gs-common/aoc/conf/init.aoc.daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
-
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 	device/google/gs-common/proprietary/aocx.IAocx.xml
-
-# AoC debug support
-PRODUCT_PACKAGES_DEBUG += \
-	aocdump \
-	aocutil \
-	aoc_audio_cfg \
-	vp_util \
-	aocx_tool
