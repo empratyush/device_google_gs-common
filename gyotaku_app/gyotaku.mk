db5ee35462cabfeed961b041eb25eb2c908b2a0a
Fred Underwood <underwoodfred@proton.me>
2025-06-15 15:53:13 +1000

    strip gyotaku_app

--- gyotaku_app/gyotaku.mk
+++ gyotaku_app/gyotaku.mk
@@ -1,13 +1,9 @@
 ifneq ($(TARGET_BUILD_VARIANT), user)
   ifeq (,$(filter aosp_%, $(TARGET_PRODUCT))) # Skip aosp target product
-    PRODUCT_PACKAGES_DEBUG += \
-      Gyotaku
-
       BOARD_SEPOLICY_DIRS += device/google/gs-common/gyotaku_app/sepolicy/
 
       # Pixel 5a (barbet) does not support Pixel dump
       ifeq (,$(filter barbet%,$(TARGET_PRODUCT)))
-        PRODUCT_PACKAGES_DEBUG += dump_gyotaku
         BOARD_SEPOLICY_DIRS += device/google/gs-common/gyotaku_app/dump
       endif
   endif
--- gyotaku_app/gyotaku_fingerprint.mk
+++ gyotaku_app/gyotaku_fingerprint.mk
@@ -1,5 +1,1 @@
-# Specific build for fingerprint
-PRODUCT_PACKAGES_DEBUG += \
-   Gyotaku
-
 BOARD_SEPOLICY_DIRS += device/google/gs-common/gyotaku_app/fingerprint
