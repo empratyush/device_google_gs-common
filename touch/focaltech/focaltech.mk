f33ef6ebc75d4ec1503e5869f2e345b472151a03
Fred Underwood <underwoodfred@proton.me>
2025-06-15 17:55:41 +1000

    strip touch/focaltech

--- touch/focaltech/focaltech.mk
+++ touch/focaltech/focaltech.mk
@@ -1,3 +1,1 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/touch/focaltech/sepolicy
-
-PRODUCT_PACKAGES += dump_focaltech.sh
--- touch/focaltech/predump_focaltech.mk
+++ touch/focaltech/predump_focaltech.mk
@@ -1,3 +1,1 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/touch/focaltech/predump_sepolicy
-
-PRODUCT_PACKAGES += predump_focaltech.sh
